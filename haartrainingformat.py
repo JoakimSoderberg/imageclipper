292cdd9db8250e14de3bcc0e6c5420c462445577
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-05-18 17:25:09 +0200

    Added a python script that creates an info file for createsamples.
    The createsamples application takes the following input for positive images:
    some/path/img.png x y w h
    
    This scripts parses the output imageclipper creates (hard coded to use the default for imageclipper -i format at the moment) and then outputs result to stdout.

---
+++ haartrainingformat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+import re
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("--clipdir", metavar = "CLIPDIR", 
+					help = "Path to a directory containing the clipped images "
+							"in the format default imageclipper output format: "
+							"<imgout_format = %%d/imageclipper/\%%i.%%e_%%04r_%%04x_%%04y_%%04w_%%04h.png>")
+
+	parser.add_argument("--path", metavar = "PATH", default = "",
+					help = "The path to prepend to the image name in the output.")
+
+	args = parser.parse_args()
+
+	clip_images = os.listdir(args.clipdir)
+
+	for img in clip_images:
+		try:
+			r = re.match("(.*?\..*?)_\d+_(\d+)_(\d+)_(\d+)_(\d+)\..*?", img)
+			basename = r.group(1)
+			x = int(r.group(2))
+			y = int(r.group(3))
+			w = int(r.group(4))
+			h = int(r.group(5))
+			print("%s %d %d %d %d" % (os.path.join(args.path, basename), x, y, w, h))
+		except Exception as ex:
+			print ex
+
+
+if __name__ == '__main__': main()
