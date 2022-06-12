6d438dcbf1f57f442e06f7e39287868e6c5c75c8
Graeme Kruger <graemekruger@gmail.com>
2022-06-12 13:28:16 +0200

    Fixes to run with OpenCV 3.14
    This also fixes
    https://github.com/JoakimSoderberg/imageclipper/issues/22, which I
    assume is related to the newer OpenCV version as well.

--- src/opencvx/cvdrawrectangle.h
+++ src/opencvx/cvdrawrectangle.h
@@ -36,14 +36,14 @@
 CVAPI(void) cvDrawRectangle(IplImage* img,
 	CvRect32f rect32f = cvRect32f(0, 0, 1, 1, 0),
 	CvPoint2D32f shear = cvPoint2D32f(0, 0),
-	CvScalar color = CV_RGB(255, 255, 255),
+	CvScalar color = cvScalar(255, 255, 255),
 	int thickness = 1,
 	int line_type = 8,
 	int shift = 0);
 CVAPI(void) cvShowImageAndRectangle(const char* w_name, const IplImage* img,
 	CvRect32f rect32f = cvRect32f(0, 0, 1, 1, 0),
 	CvPoint2D32f shear = cvPoint2D32f(0, 0),
-	CvScalar color = CV_RGB(255, 255, 0),
+	CvScalar color = cvScalar(0, 255, 255),
 	int thickness = 1, int line_type = 8,
 	int shift = 0);
 
